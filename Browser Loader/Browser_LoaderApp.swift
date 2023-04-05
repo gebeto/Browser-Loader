22e8ae3317c174838798aea579963e4f6170a07f
gebeto <slavik.nychkalo@gmail.com>
2023-04-05 19:59:00 +0300

    Initial Commit

---
+++ Browser Loader/Browser_LoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Browser_LoaderApp.swift
+//  Browser Loader
+//
+//  Created by Slavik Nychkalo on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Browser_LoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
