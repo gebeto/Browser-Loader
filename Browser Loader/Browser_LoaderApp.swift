d60e880443d2f9229e9a4370e4c65374cfe17053
gebeto <slavik.nychkalo@gmail.com>
2023-04-08 15:16:55 +0300

    create project

--- Browser Loader/Browser_LoaderApp.swift
+++ Browser Loader/Browser_LoaderApp.swift
@@ -5,13 +5,44 @@
 //  Created by Slavik Nychkalo on 05.04.2023.
 //
 
+import Foundation
 import SwiftUI
+import Cocoa
+
+
+class AppDelegate: NSResponder, NSApplicationDelegate {
+    func openUrl(bundleId: String, urls: [URL]) {
+        let browserURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: bundleId)!
+        
+        NSWorkspace.shared.open(urls, withApplicationAt: browserURL, configuration: NSWorkspace.OpenConfiguration(), completionHandler: nil)
+    }
+    
+    func application(_ application: NSApplication, open urls: [URL]) {
+        let browsers = [
+            "com.apple.Safari",
+            "com.google.Chrome",
+            "org.mozilla.firefox"
+        ]
+        let defaultBrowser = browsers[0]
+        for browser in browsers {
+            if NSWorkspace.shared.runningApplications.contains(where: { $0.bundleIdentifier == browser }) {
+                self.openUrl(bundleId: browser, urls: urls)
+                return
+            }
+        }
+        self.openUrl(bundleId: defaultBrowser, urls: urls)
+    }
+}
+
 
 @main
-struct Browser_LoaderApp: App {
+struct MyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
+
